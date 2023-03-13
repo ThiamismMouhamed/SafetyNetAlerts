3892f52a60a7594f99f5bc7eea6b5220e6e975a8
ThiamismMouhamed <ameth.thiam@univ-thies.sn>
2023-03-14 00:35:25 +0100

    complement service personne et ajout du fichier data.json

--- src/main/java/com/example/safetynetalerts/service/personne/PersonneIn.java
+++ src/main/java/com/example/safetynetalerts/service/personne/PersonneIn.java
@@ -1,7 +1,13 @@
 package com.example.safetynetalerts.service.personne;
 
+import com.example.safetynetalerts.dto.ChildListAndFamilyListDto;
+import com.example.safetynetalerts.dto.FamiliesByStationDto;
+import com.example.safetynetalerts.dto.PersonByFirstNameAndLastNameDto;
 import com.example.safetynetalerts.model.Personne;
+import org.w3c.dom.stylesheets.LinkStyle;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public interface PersonneIn {
@@ -11,6 +17,23 @@ public interface PersonneIn {
     void deletePersonne(Personne personne);
 
     Set<String> getAllMailsByCity(String city);
+    List<PersonByFirstNameAndLastNameDto> getPersonsByFirstNameAndLastName(String firstName, String lastName);
+
+    /**
+     * This method using firestation number return persons by addresses
+     *
+     * @param stations define the number of the firestation, one or more stations can be used
+     * @return This method return a map of persons by address for flood alert return by address list of persons with their firstName, lastName, phone, age, list of medications and list of allergies
+     */
+    Map<String, List<FamiliesByStationDto>> getPersonsByAddressStationForFloodAlert(List<Integer> stations);
+
+    /**
+     * Method used to get all children by address and their family, and the number of child and number of adults
+     *
+     * @param address used to filter persons
+     * @return a list of child with firstName, lastName, phone and age and a list of Adults with same description
+     */
+    ChildListAndFamilyListDto getChildrenByAddress(String address);
 
     
 }
--- src/main/java/com/example/safetynetalerts/service/personne/PersonneService.java
+++ src/main/java/com/example/safetynetalerts/service/personne/PersonneService.java
@@ -1,14 +1,16 @@
 package com.example.safetynetalerts.service.personne;
 
+import com.example.safetynetalerts.dto.*;
 import com.example.safetynetalerts.exeptions.BadRequestExeptions;
+import com.example.safetynetalerts.model.Firestation;
+import com.example.safetynetalerts.model.MedicalRecord;
 import com.example.safetynetalerts.model.Personne;
 import com.example.safetynetalerts.service.dataStorage.DataStorageService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
@@ -102,4 +104,112 @@ public class PersonneService implements PersonneIn{
                 .map(Personne::getEmail)
                 .collect(Collectors.toSet());
     }
+
+    @Override
+    public List<PersonByFirstNameAndLastNameDto> getPersonsByFirstNameAndLastName(String firstName, String lastName) {
+        log.debug("Get all persons by address: " + firstName + " " + lastName);
+
+        return dataStorageService.getPersonnes()
+                .stream()
+                .filter(p -> lastName.equals(p.getLastName()))
+                .map(person -> dataStorageService.getMedicalrecords()
+                        .stream()
+                        .filter(medicalrecord -> medicalrecord.getId().equals(person.getId()))
+                        .map(medicalrecord -> new PersonByFirstNameAndLastNameDto(person, medicalrecord))
+                        .findFirst().orElse(null))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Map<String, List<FamiliesByStationDto>> getPersonsByAddressStationForFloodAlert(List<Integer> stations) {
+        log.debug("Get all persons by address: " + stations);
+
+        List<MedicalRecord> medicalrecords = dataStorageService.getMedicalrecords();
+
+        List<String> allAddressesByStationNumber = new ArrayList<>();
+        Map<String, List<FamiliesByStationDto>> allPersons = new HashMap<>();
+
+        for(Integer firestationNumber : stations) {
+            List<String> getAllAddressesByStationNumber =
+                    dataStorageService
+                            .getFirestations()
+                            .stream()
+                            .filter(firestation -> firestation.getStation() == firestationNumber)
+                            .map(Firestation::getAddress)
+                            .collect(Collectors.toList());
+            allAddressesByStationNumber.addAll(getAllAddressesByStationNumber);
+            log.info("get all addresses by station number");
+        }
+
+        for(String address : allAddressesByStationNumber) {
+            List<FamiliesByStationDto> getAllPersonsWithThisAddress =
+                    dataStorageService
+                            .getPersonnes()
+                            .stream()
+                            .filter(person -> person.getAddress().equals(address))
+                            .map(person -> {
+                                        MedicalRecord m = medicalrecords
+                                                .stream()
+                                                .filter(medicalRecord -> medicalRecord.getId().equals(person.getId()))
+                                                .findFirst()
+                                                .orElseThrow(() -> new BadRequestExeptions(""));
+                                        return new FamiliesByStationDto(person, m);
+                                    }
+                            )
+                            .collect(Collectors.toList());
+            allPersons.put(address, getAllPersonsWithThisAddress);
+            log.info("Get all persons by address");
+        }
+
+        return allPersons;
+    }
+
+    @Override
+    public ChildListAndFamilyListDto getChildrenByAddress(String address) {
+        log.debug("Get children by address: " + address);
+
+        List<MedicalRecord> medicalrecords = dataStorageService.getMedicalrecords();
+
+        List<ChildrenByAddressDto> getChild = new ArrayList<>();
+        List<AdultsByAddressDto> getAdults = new ArrayList<>();
+        ChildListAndFamilyListDto childListAndFamilyListDto = new ChildListAndFamilyListDto(getChild, getAdults);
+
+        for(MedicalRecord medicalRecord : medicalrecords) {
+            List<ChildrenByAddressDto> getChildrenByAddress =
+                    dataStorageService
+                            .getPersonnes()
+                            .stream()
+                            .filter(person -> person.getId().equals(medicalRecord.getId()))
+                            .filter(person -> person.getAddress().equals(address))
+                            .map(person -> new ChildrenByAddressDto(person, medicalRecord))
+                            .filter(ChildrenByAddressDto::isMinor)
+                            .collect(Collectors.toList());
+
+            getChild.addAll(getChildrenByAddress);
+            log.info("Get child by address");
+        }
+
+        for(MedicalRecord medicalrecord : medicalrecords) {
+            if(!getChild.isEmpty()) {
+                List<AdultsByAddressDto> getAdultByAddress =
+                        dataStorageService
+                                .getPersonnes()
+                                .stream()
+                                .filter(person -> person.getId().equals(medicalrecord.getId()))
+                                .filter(person -> person.getAddress().equals(address))
+                                .map(person -> new AdultsByAddressDto(person, medicalrecord))
+                                .filter(AdultsByAddressDto::iSMajor)
+                                .collect(Collectors.toList());
+
+                getAdults.addAll(getAdultByAddress);
+                log.info("Get adults by address");
+            }
+        }
+
+        childListAndFamilyListDto.setGetChildrenByAddressDto(getChild);
+        childListAndFamilyListDto.setGetAdultsByAddressDto(getAdults);
+        log.info("List of child by address and adult by address");
+        return childListAndFamilyListDto;
+    }
+
 }
