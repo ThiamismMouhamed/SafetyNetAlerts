d73d6366b1245817a2c43c462cd4c55028b23d66
ThiamismMouhamed <ameth.thiam@univ-thies.sn>
2023-03-14 00:50:57 +0100

    complement interface MedicalRecordIn et MedicalRecordService

--- src/main/java/com/example/safetynetalerts/service/firestation/FirestationIn.java
+++ src/main/java/com/example/safetynetalerts/service/firestation/FirestationIn.java
@@ -1,11 +1,42 @@
 package com.example.safetynetalerts.service.firestation;
 
+import com.example.safetynetalerts.dto.NumberOfAdultsAndChildrenDto;
+import com.example.safetynetalerts.dto.PersonsByAddressDto;
+import com.example.safetynetalerts.dto.PersonsByStationDto;
 import com.example.safetynetalerts.model.Firestation;
+
+import java.util.List;
+import java.util.Set;
 
 public interface FirestationIn {
 
     void addMedicalRecord(Firestation firestation);
     void updateFirestation(Firestation firestation);
     void deleteFirestation(Firestation firestation);
+    List<PersonsByStationDto> getPersonsByStation(int stationNumber);
+
+    /**
+     * Method used to return phone number
+     *
+     * @param station is the station number
+     * @return a list of phone number without duplicate serve by this station
+     */
+    Set<String> getPhoneByFirestationNumber(int station);
+
+    /**
+     * Used to return persons by address
+     *
+     * @param address is the address served by the fire station
+     * @return a list of persons served at this address return firstName, lastName, age, list of medications, list of allergies and fire station number
+     */
+    List<PersonsByAddressDto> getPersonsByAddress(String address);
+
+    /**
+     * Return number of adults and children
+     *
+     * @param station is the number of the fire station
+     * @return the number of adults and child served by the fire station by station in parameters
+     */
+    NumberOfAdultsAndChildrenDto getNumbersOfChildrenAndAdultsByStation(int station);
 
 }
--- src/main/java/com/example/safetynetalerts/service/firestation/FirestationService.java
+++ src/main/java/com/example/safetynetalerts/service/firestation/FirestationService.java
@@ -1,13 +1,20 @@
 package com.example.safetynetalerts.service.firestation;
 
+import com.example.safetynetalerts.dto.NumberOfAdultsAndChildrenDto;
+import com.example.safetynetalerts.dto.PersonsByAddressDto;
+import com.example.safetynetalerts.dto.PersonsByStationDto;
 import com.example.safetynetalerts.exeptions.BadRequestExeptions;
 import com.example.safetynetalerts.model.Firestation;
+import com.example.safetynetalerts.model.MedicalRecord;
+import com.example.safetynetalerts.model.Personne;
 import com.example.safetynetalerts.service.dataStorage.DataStorageService;
 import com.example.safetynetalerts.service.medicalrecord.MedicalRecordService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Component
 @Log4j2
 public class FirestationService implements FirestationIn{
@@ -88,5 +95,106 @@ public class FirestationService implements FirestationIn{
             throw new BadRequestExeptions("Firestation who serve this address doesn't exist");
         }
 
+    }
+
+    @Override
+    public List<PersonsByStationDto> getPersonsByStation(int stationNumber) {
+        log.debug("Get persons by firestation number: " + stationNumber);
+
+        log.info("Get firestation address by station number");
+        List<String> firestationAddressByStationNumber =
+                dataStorageService
+                        .getFirestationsByNumber(stationNumber)
+                        .stream()
+                        .map(Firestation::getAddress)
+                        .collect(Collectors.toList());
+
+
+        log.info("Get all persons by address");
+
+        return dataStorageService
+                .getPersonnes()
+                .stream()
+                .filter(person -> firestationAddressByStationNumber.contains(person.getAddress()))
+                .map(PersonsByStationDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Set<String> getPhoneByFirestationNumber(int station) {
+        log.debug("Get phone by firestation number" + station);
+
+        List<String> firestationAddress =
+                dataStorageService
+                        .getFirestationsByNumber(station)
+                        .stream()
+                        .map(Firestation::getAddress)
+                        .collect(Collectors.toList());
+        log.info("Phone number by firestation number has been recovered");
+        return dataStorageService
+                .getPersonnes()
+                .stream()
+                .filter(p -> firestationAddress.contains(p.getAddress()))
+                .map(Personne::getPhone)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public List<PersonsByAddressDto> getPersonsByAddress(String address) {
+        log.debug("Get persons by address" + address);
+
+        List<Personne> persons = dataStorageService.getPersonneByAdresse(address);
+
+        List<MedicalRecord> medicalrecords = dataStorageService.getMedicalrecords();
+
+        List<Firestation> fireStations =
+                dataStorageService
+                        .getFirestationsByAddress(address);
+
+        List<PersonsByAddressDto> personsByAddressDto = new ArrayList<>();
+
+        for(Personne person : persons) {
+            List<PersonsByAddressDto> aggregate =
+                    fireStations
+                            .stream()
+                            .map(fireStation -> {
+                                try {
+                                    return new PersonsByAddressDto(person, fireStation, Objects.requireNonNull(medicalrecords
+                                            .stream()
+                                            .filter(medicalRecord -> medicalRecord.getId().equals(person.getId()))
+                                            .findFirst().orElse(null)));
+                                } catch(Exception e) {
+                                    e.printStackTrace();
+                                }
+                                return null;
+                            })
+                            .collect(Collectors.toList());
+
+            personsByAddressDto.addAll(aggregate);
+            log.info("All persons by address has been recovered");
+        }
+
+        return personsByAddressDto;
+    }
+
+    @Override
+    public NumberOfAdultsAndChildrenDto getNumbersOfChildrenAndAdultsByStation(int station) {
+        log.debug("Get numbers of adults and children by station");
+        int adultsNumber = 0;
+        int childrenNumber = 0;
+
+        for(PersonsByStationDto person : getPersonsByStation(station)) {
+
+            Optional<MedicalRecord> medicalrecord = medicalRecordService.getMedicalRecordByPersonneId(person.getId());
+
+            if(medicalrecord.isPresent()) {
+                Integer personAge = medicalrecord.get().getAge();
+                if(personAge > 18) {
+                    adultsNumber++;
+                } else childrenNumber++;
+            }
+        }
+        log.info("Return number of children and adults");
+        return new NumberOfAdultsAndChildrenDto(childrenNumber, adultsNumber);
     }
 }
