3cbc9ecf1ceafad2a6ae14fe54f31a44efc22f75
ThiamismMouhamed <ameth.thiam@univ-thies.sn>
2023-03-14 01:05:35 +0100

    implementation des controllers AlertController et EmailController

--- src/main/java/com/example/safetynetalerts/controller/AlertController.java
+++ src/main/java/com/example/safetynetalerts/controller/AlertController.java
@@ -1,11 +1,19 @@
 package com.example.safetynetalerts.controller;
 
+import com.example.safetynetalerts.dto.ChildListAndFamilyListDto;
+import com.example.safetynetalerts.dto.FamiliesByStationDto;
+import com.example.safetynetalerts.dto.PersonsByAddressDto;
 import com.example.safetynetalerts.service.firestation.FirestationService;
 import com.example.safetynetalerts.service.personne.PersonneService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 public class AlertController {
@@ -16,6 +24,50 @@ public class AlertController {
     public AlertController(FirestationService firestationService, PersonneService personneService) {
         this.firestationService = firestationService;
         this.personneService = personneService;
+    }
+
+    /**
+     * Gets persons by address.
+     *
+     * @param address the address
+     * @return the persons by address
+     */
+    @GetMapping("/fire")
+    ResponseEntity<List<PersonsByAddressDto>> getPersonsByAddress(@RequestParam String address) {
+        return new ResponseEntity<>(firestationService.getPersonsByAddress(address), HttpStatus.OK);
+    }
+
+    /**
+     * Gets phone numbers by firestation number.
+     *
+     * @param firestation the firestation
+     * @return the phone numbers by firestation number
+     */
+    @GetMapping("/phoneAlert")
+    ResponseEntity<Set<String>> getPhoneNumbersByFirestationNumber(@RequestParam int firestation) {
+        return new ResponseEntity<>(firestationService.getPhoneByFirestationNumber(firestation), HttpStatus.OK);
+    }
+
+    /**
+     * Gets child by address.
+     *
+     * @param address the address
+     * @return the child by address
+     */
+    @GetMapping("/childAlert")
+    ResponseEntity<ChildListAndFamilyListDto> getChildByAddress(@RequestParam String address) {
+        return new ResponseEntity<>(personneService.getChildrenByAddress(address), HttpStatus.OK);
+    }
+
+    /**
+     * Gets persons by address station for flood alert.
+     *
+     * @param stations the stations
+     * @return the persons by address station for flood alert
+     */
+    @GetMapping("/flood/stations/")
+    ResponseEntity<Map<String, List<FamiliesByStationDto>>> getPersonsByAddressStationForFloodAlert(@RequestParam List<Integer> stations) {
+        return new ResponseEntity<>(personneService.getPersonsByAddressStationForFloodAlert(stations), HttpStatus.OK);
     }
 
 
--- src/main/java/com/example/safetynetalerts/controller/EmailController.java
+++ src/main/java/com/example/safetynetalerts/controller/EmailController.java
@@ -1,4 +1,37 @@
 package com.example.safetynetalerts.controller;
 
+import com.example.safetynetalerts.service.personne.PersonneService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
 public class EmailController {
+
+    private final PersonneService personneService;
+
+    /**
+     * Instantiates a new Community email controller.
+     *
+     * @param personneService the person manager
+     */
+    public EmailController(PersonneService personneService) {
+        this.personneService = personneService;
+    }
+
+
+    /**
+     * Gets all mails by city.
+     *
+     * @param city the city
+     * @return the all mails by city
+     */
+    @GetMapping("/communityEmail")
+    ResponseEntity<Set<String>> getAllMailsByCity(@RequestParam String city) {
+        return new ResponseEntity<>(personneService.getAllMailsByCity(city), HttpStatus.OK);
+    }
 }
